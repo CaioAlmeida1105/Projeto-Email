fd429e35b75ff20165fe646495f11c1be436c4bc
Caio Almeida dos Santos <santos.almeida.caio@gmail.com>
2024-10-29 21:13:14 -0300

    funcoes-de-email

--- projeto-email.py
+++ projeto-email.py
@@ -1,21 +1,29 @@
+#IMPORTANDO BIBLIOTECAS
+import imap_tools as it
+import imaplib as il
 
+#COLETANDO INFORMAÇÕES
+usuario = str(input("Informe seu endereço de E-mail:"))
+senha = str(input("Informe sua senha:"))
+servidor = str(input("Informe seu servidor (ex: imap.gmail.com):"))
+pasta = str(input("Informe a pasta do seu E-mail que você deseja selecionar:"))
+meu_email = il.IMAP4_SSL(servidor)
 
-#ENTRAR NO E-MAIL
+#FUNÇÃO DE LOGAR AO SERVIDOR
+def logar():
+    meu_email.login(usuario, senha)
+    return meu_email
 
+#FUNÇÃO DE CONTAR E-MAILS NÃO LIDOS
+def contando_emails():
+    meu_email.select(pasta)
+    status, emails = meu_email.search(None, 'UNSEEN')
+    if status == 'OK':
+        email_ids = emails[0].split()
+        print(f"Total de e-mails não lidos na pasta '{pasta}': {len(email_ids)}")
+    else:
+        print("Não foi possível acessar a pasta.")
 
-#PROCURAR A PASTA
-
-
-#CONTAR QUANTOS E-MAILS NÃO LIDOS TEM NA PASTA
-
-
-#PEGAR A PLANILHA
-
-
-#REGISTRAR NA PLANILHA 
-
-
-#SEPARAR POR HORARIOS
-
-
-#REFAZER O PROCESSO NAS OUTRAS PASTAS
+#APLICANDO FUNÇÕES
+logar()
+contando_emails()
